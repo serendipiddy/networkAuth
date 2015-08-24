72355903e47d52e658c236690ac0093636e07c2e
iddy-489vm <iddyiddyiddy@gmail.com>
2015-08-24 19:06:52 +1200

    hubswitch written, bugs removed

--- simple_hubswitch.py
+++ simple_hubswitch.py
@@ -111,16 +111,16 @@ class SimpleHubSwitch(app_manager.RyuApp):
         if out_port != ofp.OFPP_FLOOD:
           match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
           
-          if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-            self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+          if msg.buffer_id != ofp.OFP_NO_BUFFER:
+            self.add_flow(dp, 1, match, actions, msg.buffer_id)
             return
           else:
-            self.add_flow(datapath, 1, match, actions)
+            self.add_flow(dp, 1, match, actions)
         
         data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+        if msg.buffer_id == ofp.OFP_NO_BUFFER:
             data = msg.data
 
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+        out = parser.OFPPacketOut(datapath=dp, buffer_id=msg.buffer_id,
                                   in_port=in_port, actions=actions, data=data)
-        datapath.send_msg(out)
+        dp.send_msg(out)
